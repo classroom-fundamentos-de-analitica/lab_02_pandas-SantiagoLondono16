89ad62b2da48d4357f3433ed95e74df62906f397
Santiago Londoño Giraldo <slondonogi@unal.edu.co>
2024-09-01 13:11:19 -0500

    *

--- preguntas.py
+++ preguntas.py
@@ -7,7 +7,6 @@ Este archivo contiene las preguntas que se van a realizar en el laboratorio.
 Utilice los archivos `tbl0.tsv`, `tbl1.tsv` y `tbl2.tsv`, para resolver las preguntas.
 
 """
-
 import pandas as pd
 
 tbl0 = pd.read_csv("tbl0.tsv", sep="\t")
@@ -23,7 +22,7 @@ def pregunta_01():
     40
 
     """
-    return
+    return len(tbl0)
 
 
 def pregunta_02():
@@ -34,7 +33,8 @@ def pregunta_02():
     4
 
     """
-    return
+
+    return len(tbl0.columns)
 
 
 def pregunta_03():
@@ -51,7 +51,8 @@ def pregunta_03():
     Name: _c1, dtype: int64
 
     """
-    return
+    
+    return tbl0["_c1"].value_counts().sort_index()
 
 
 def pregunta_04():
@@ -66,7 +67,8 @@ def pregunta_04():
     E    4.785714
     Name: _c2, dtype: float64
     """
-    return
+
+    return tbl0.groupby("_c1")["_c2"].mean()
 
 
 def pregunta_05():
@@ -83,7 +85,8 @@ def pregunta_05():
     E    9
     Name: _c2, dtype: int64
     """
-    return
+
+    return tbl0.groupby("_c1")["_c2"].max()
 
 
 def pregunta_06():
@@ -95,7 +98,8 @@ def pregunta_06():
     ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
     """
-    return
+
+    return tbl1["_c4"].str.upper().sort_values().unique()
 
 
 def pregunta_07():
@@ -111,7 +115,8 @@ def pregunta_07():
     E    67
     Name: _c2, dtype: int64
     """
-    return
+
+    return tbl0.groupby("_c1")["_c2"].sum()
 
 
 def pregunta_08():
@@ -129,7 +134,12 @@ def pregunta_08():
     39   39   E    5  1998-01-26    44
 
     """
-    return
+
+    tbl0["suma"] = tbl0["_c0"] + tbl0["_c2"]
+
+    tbl0.to_csv('tbl0.tsv', index=False, sep="\t")
+
+    return print(tbl0)
 
 
 def pregunta_09():
@@ -147,7 +157,11 @@ def pregunta_09():
     39   39   E    5  1998-01-26  1998
 
     """
-    return
+
+    tbl0["year"] = tbl0["_c3"].apply(lambda x: x.split("-")[0])
+    tbl0.to_csv('tbl0.tsv', index=False, sep="\t")
+
+    return print(tbl0)
 
 
 def pregunta_10():
@@ -164,7 +178,8 @@ def pregunta_10():
     3   D                  1:2:3:5:5:7
     4   E  1:1:2:3:3:4:5:5:5:6:7:8:8:9
     """
-    return
+
+    return tbl0.sort_values(["_c1", "_c2"]).groupby("_c1")["_c2"].apply(lambda x: ":".join(map(str, x))).reset_index()
 
 
 def pregunta_11():
@@ -183,7 +198,9 @@ def pregunta_11():
     38   38      d,e
     39   39    a,d,f
     """
-    return
+    
+    return tbl1.sort_values(["_c0", "_c4"]).groupby("_c0")["_c4"].apply(lambda x: ",".join(map(str, x))).reset_index()
+
 
 
 def pregunta_12():
@@ -201,7 +218,10 @@ def pregunta_12():
     38   38                    eee:0,fff:9,iii:2
     39   39                    ggg:3,hhh:8,jjj:5
     """
-    return
+
+    tbl2["ab"] = tbl2["_c5a"] + ":" + tbl2["_c5b"].astype(str)
+
+    return tbl2.sort_values(["_c0", "_c5a"]).groupby("_c0")["ab"].apply(lambda x: ",".join(map(str, x))).reset_index()
 
 
 def pregunta_13():
@@ -218,4 +238,7 @@ def pregunta_13():
     E    275
     Name: _c5b, dtype: int64
     """
-    return
+
+    merged = pd.merge(tbl0, tbl2, left_on="_c0", right_on="_c0")
+
+    return merged.groupby('_c1')['_c5b'].sum()
